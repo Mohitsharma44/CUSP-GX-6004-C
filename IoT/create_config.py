342eada316648f4e6d86a698ed0a7bd1dd85196c
mohitsharma44 <mohitsharma44@gmail.com>
2017-05-18 17:30:14 -0400

    changed folder structure so that its easier to copy

--- IoT/create_config.py
+++ IoT/create_config.py
@@ -11,6 +11,7 @@ from datetime import datetime
 if __name__ == "__main__":
 
     config_file = "./setup/raspberrypi-ua-netinst-v1.5.1/raspberrypi-ua-netinst/config/installer-config.txt"
+    destination = "files/root/home/pi/"
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-hosts', action='store', dest='total_nodes',
@@ -21,10 +22,13 @@ if __name__ == "__main__":
 
     all_hosts = list(range(50, 50+int(results.total_nodes)))
     _all_ips  = results.network.rstrip('0')+"{host}"
-    all_ips  = list(map(lambda x: _all_ips.format(host=x), all_hosts))
+    all_ips  = list(map(lambda x: _all_ips.format(host=x),
+                        sorted(all_hosts, reverse=True)))
 
     for host in all_hosts:
-        path = './{current_host}'.format(current_host=host)
+        file_path = os.path.join('./{current_host}'.format(current_host=host),
+                            destination)
+        installer_path = os.path.join('./{current_host}'.format(current_host=host))
         # parse the config file to a dictionary
         conf = {}
         with open(config_file) as fh:
@@ -43,18 +47,20 @@ if __name__ == "__main__":
         # generate 2048bit key
         key = RSA.generate(2048)
         # write private key to the file
-        os.mkdir(path)
-        with open(os.path.join(path, 'private.key'), 'w') as pvtkey_file:
+        os.makedirs(path, exist_ok=True)
+        with open(os.path.join(file_path, 'private.key'), 'wb') as pvtkey_file:
             pvtkey_file.write(key.exportKey('PEM'))
-            os.chmod(os.path.join(path, 'private.key'), 0600)
+            os.chmod(os.path.join(path, 'private.key'), 600)
         # change this in config file as well
         if conf.get('user_ssh_pubkey', None):
             conf['user_ssh_pubkey'] = key.publickey().exportKey('OpenSSH')
 
         # write the configuration to a file
-        with open(os.path.join(path, 'installer-config.txt'), 'w') as cfg:
+        with open(os.path.join(installer_path, 'installer-config.txt'), 'w') as cfg:
             for item in conf.items():
                 cfg.write("{key}={value}\n".format(key=item[0], value=item[1]))
+
+        # -- Thats It!
         print("[{time}]: Done configuration for {host}".format(
             time=datetime.now(),
             host=host))
