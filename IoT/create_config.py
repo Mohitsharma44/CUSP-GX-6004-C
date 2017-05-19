ccc1fbdf7ffc350b1b8c0244dc9bd766168e1f49
mohitsharma44 <mohitsharma44@gmail.com>
2017-05-19 17:41:59 -0400

    changed location of installer config

--- IoT/create_config.py
+++ IoT/create_config.py
@@ -10,17 +10,19 @@ from datetime import datetime
 
 if __name__ == "__main__":
 
-    config_file = "./setup/raspberrypi-ua-netinst-v1.5.1/raspberrypi-ua-netinst/config/installer-config.txt"
-    destination = "files/root/home/pi/"
+    config_file = "./installer-config.txt"
+    destination = "files/pi/"
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-hosts', action='store', dest='total_nodes',
                         help='total number of nodes for which the configuration needs to be generated')
     parser.add_argument('-net', action='store', dest='network',
                         help='Network e.g. 192.168.1.0. Subnet is assumed to be /24')
+    parser.add_argument('-st', action='store', dest='start_from',
+                        help='start the host naming from. The nodes will be numbered sequentially')
     results = parser.parse_args()
 
-    all_hosts = list(range(50, 50+int(results.total_nodes)))
+    all_hosts = list(range(int(results.start_from), int(results.start_from)+int(results.total_nodes)))
     _all_ips  = results.network.rstrip('0')+"{host}"
     all_ips  = list(map(lambda x: _all_ips.format(host=x),
                         sorted(all_hosts, reverse=True)))
@@ -50,7 +52,7 @@ if __name__ == "__main__":
         os.makedirs(file_path, exist_ok=True)
         with open(os.path.join(file_path, 'private.key'), 'wb') as pvtkey_file:
             pvtkey_file.write(key.exportKey('PEM'))
-            os.chmod(os.path.join(file_path, 'private.key'), 600)
+            os.chmod(os.path.join(file_path, 'private.key'), 0o600)
         # change this in config file as well
         if conf.get('user_ssh_pubkey', None):
             conf['user_ssh_pubkey'] = key.publickey().exportKey('OpenSSH')
