6d30f8d7918cacb24ab9eb367b88635067b6ce12
mohitsharma44 <mohitsharma44@gmail.com>
2017-07-12 09:46:41 -0400

    fixed. Serving keys now

--- assignKey.py
+++ assignKey.py
@@ -15,22 +15,23 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("index.html", title="", myKey="")
 
     def post(self):
-        paths = glob.glob(os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys") + "/*.key")
-        #map(lambda x: os.path.basename(x)[:-4], paths)
-        keys = [os.path.basename(k)[:-4] for k in paths]
-        #dict_key_netid = {dk:0 for dk in keys}
         dict_key_netid = {}
+        keys  = None
+        paths = glob.glob(os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "keys") + "/*.key")
+        keys  = [os.path.basename(k)[:-4] for k in paths]
         netid = self.get_argument("netid")
-        validate = 0
-        checkId = pd.read_csv("CUSP_F17_Students.csv", usecols=[2])
+        validate = False
+        checkId  = pd.read_csv("CUSP_F17_Students.csv", usecols=[2])
         if (checkId["NYU NetID"].str.contains(netid).any()):
-            validate = 1
-        if(validate == 1):
-            if ((netid not in dict_key_netid) and len(keys)):
+            validate = True
+        if validate:
+            if ((netid not in dict_key_netid) and keys):
                 assignKey = random.choice(keys)
                 dict_key_netid[netid] = assignKey
                 keys.pop(keys.index(assignKey))
-                path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys", assignKey) + ".key"
+                path = os.path.join("keys", assignKey) + ".key"
                 self.render("index.html", title = "IOT Keys", myKey = path)
             else:
                 keyFound = dict_key_netid[netid]
@@ -48,7 +49,7 @@ settings = {
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/(.*)", tornado.web.StaticFileHandler, {'path':"/home/himanshu/Documents/tornadoFileServer/keys"}),
+        (r"/keys/(.*)", tornado.web.StaticFileHandler,{'path': "./keys"}),
     ],
     **settings
 )
