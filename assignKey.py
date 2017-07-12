6645c350794293012d6d6557c2e54766918e248e
mohitsharma44 <mohitsharma44@gmail.com>
2017-07-12 13:35:43 -0400

    working prototype

--- assignKey.py
+++ assignKey.py
@@ -1,18 +1,77 @@
-import tornado.ioloop
-import tornado.web
-import tornado.httpserver
-import pandas as pd
-import random
-import glob
 import os
+import glob
+import random
+import pandas as pd
+from tornado import websocket, web, ioloop, gen, escape
 
-class MainHandler(tornado.web.RequestHandler):
+AUTHORIZED_USERS = {}
+[AUTHORIZED_USERS.update(
+    {x['NYU NetID']: {'First Name': x['First Name'],
+                      'Last Name': x['Last Name']}})
+ for x in pd.read_csv(os.getenv('NetId_17csv')).to_dict(orient='records')]
+
+
+class BaseHandler(web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
+
+class MainHandler(BaseHandler):
+    @web.asynchronous
+    @web.authenticated
+    def get(self):
+        userid = escape.xhtml_escape(self.current_user)
+        self.render("index.html",
+                    title="IOTclass",
+                    # The things you need to do to convert between bytes and string!!!
+                    username=AUTHORIZED_USERS[self.current_user.decode('utf-8').strip('"')]
+                    ['First Name'])
+
+    def post(self):
+        pass
+
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        try:
+            error_msg = self.get_argument("error")
+        except Exception:
+            error_msg = ""
+        self.render("login.html", errormessage=error_msg)
+
+    def post(self):
+        userid = self.get_argument("netid", "")
+        if userid in AUTHORIZED_USERS.keys():
+            self.set_current_user(userid)
+            self.redirect(self.get_argument("next", u"/"))
+        else:
+            error_msg = u"?error=" + escape.url_escape("UserID incorrect")
+            self.redirect(u"/login" + error_msg)
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie("user", escape.json_encode(user))
+        else:
+            self.clear_cookie("user")
+
+
+class LogoutHandler(BaseHandler):
     """
-    Class to handle requests for assigning a
-    key and authentication
+    Logout user and clear the cookie
     """
     def get(self):
-        self.render("index.html", title="", myKey="")
+        self.clear_cookie("user")
+        self.redirect(self.get_argument("next", u"/"))
+
+
+class KeyDownloadHandler(BaseHandler):
+    """
+    Class to provide download of key
+    """
+    def get(self):
+        self.render("index.html", title="",
+                    login_form=True, myKey="",
+                    authenticated=False)
 
     def post(self):
         dict_key_netid = {}
@@ -31,30 +90,36 @@ class MainHandler(tornado.web.RequestHandler):
                 assignKey = random.choice(keys)
                 dict_key_netid[netid] = assignKey
                 keys.pop(keys.index(assignKey))
-                path = os.path.join("keys", assignKey) + ".key"
-                self.render("index.html", title = "IOT Keys", myKey = path)
+                path = os.path.join("../keys", assignKey) + ".key"
             else:
                 keyFound = dict_key_netid[netid]
-                path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys", keyFound) + ".key"
-                self.render("index.html", title = "IOT Keys", myKey = path)
+                path = os.path.join("../keys", keyFound) + ".key"
+            self.render("index.html", title="IOT Keys",
+                        myKey=path, login_form="True",
+                        authenticated=True)
 
 settings = {
+    'login_url': '/login',
     'template_path': 'templates/',
     'compiled_template_cache': 'False',
     'debug': True,
-    'static_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys")
+    'cookie_secret': "Mysup3rS3cr3tC00kie",
+    'static_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
 }
 
 
 def make_app():
-    return tornado.web.Application([
+    return web.Application([
         (r"/", MainHandler),
-        (r"/keys/(.*)", tornado.web.StaticFileHandler,{'path': "./keys"}),
+        (r"/login", LoginHandler),
+        (r"/logout", LogoutHandler),
+        (r"/key", KeyDownloadHandler),
+        (r"/keys/(.*)", web.StaticFileHandler,{'path': "../keys"}),
     ],
     **settings
 )
 
 if __name__ == "__main__":
-    app = tornado.httpserver.HTTPServer(make_app())
+    app = make_app()
     app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    ioloop.IOLoop.current().start()
